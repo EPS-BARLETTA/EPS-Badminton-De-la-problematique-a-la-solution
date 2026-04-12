c6f81fbd4530e79caeb919ad9b77294428696fb8
EPS-BARLETTA <j.barletta@vauban.lu>
2026-04-12 11:57:48 +0200

    Update BasesTechniques.jsx

--- src/pages/BasesTechniques.jsx
+++ src/pages/BasesTechniques.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 function BasesTechniques() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
   return (
     <section className="panel">
       <header className="section-header">
@@ -16,6 +20,7 @@ function BasesTechniques() {
             src="/images/bases-prises-1.png"
             alt="Les deux prises de raquette 1"
             className="bases-image"
+            onClick={() => setSelectedImage('/images/bases-prises-1.png')}
           />
         </article>
 
@@ -25,24 +30,27 @@ function BasesTechniques() {
             src="/images/bases-prises-2.png"
             alt="Les deux prises de raquette 2"
             className="bases-image"
+            onClick={() => setSelectedImage('/images/bases-prises-2.png')}
           />
         </article>
 
         <article className="panel">
           <h3>Placements 1</h3>
           <img
-            src="/images/bases-placement-1.png"
+            src="/images/bases-placements-1.png" {/* ✅ corrigé */}
             alt="Placements 1"
             className="bases-image"
+            onClick={() => setSelectedImage('/images/bases-placements-1.png')}
           />
         </article>
 
         <article className="panel">
           <h3>Placements 2</h3>
           <img
-            src="/images/bases-placement-2.png"
+            src="/images/bases-placements-2.png" {/* ✅ corrigé */}
             alt="Placements 2"
             className="bases-image"
+            onClick={() => setSelectedImage('/images/bases-placements-2.png')}
           />
         </article>
 
@@ -52,9 +60,17 @@ function BasesTechniques() {
             src="/images/sur-filet.png"
             alt="Comment construire un sur-filet"
             className="bases-image"
+            onClick={() => setSelectedImage('/images/sur-filet.png')}
           />
         </article>
       </div>
+
+      {/* 🔥 MODAL ZOOM */}
+      {selectedImage && (
+        <div className="image-modal" onClick={() => setSelectedImage(null)}>
+          <img src={selectedImage} alt="zoom" />
+        </div>
+      )}
     </section>
   )
 }
